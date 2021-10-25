7b6447e91fdddfb7ac9c8d6b4364c6d0fbd2678f
Brian Donnoe <dunow33@hotmail.com>
2021-10-25 19:31:08 -0400

    initial setup

--- app/src/main/java/com/example/questionairewithlivedataandviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/questionairewithlivedataandviewmodel/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
         Log.d("test", "onCreate starts from MainActivity")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
